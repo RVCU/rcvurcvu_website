7ed4ea98d939017c86f1a87215adf787c19de0ac
RCVU <reillylefort20@gmail.com>
2017-11-18 20:40:15 +0000

    website

---
+++ cssfixer.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Moves main.css into each subfolder calculus abstractalgebra and numlinalg
+
+cp -f /var/www/html/main.css /var/www/html/calculus
+cp -f /var/www/html/main.css /var/www/html/abstractalgebra
+cp -f /var/www/html/main.css /var/www/html/numlinalg
+cp -f /var/www/html/main.css /var/www/html/ComplexAnalysis
